7d5015a0425b5f6e4f3105600f299fc2371c15ab
ithkuilg <99258753+ithkuilg@users.noreply.github.com>
2022-04-25 09:48:55 +0000

    Minor changes in README

--- src/Storage.js
+++ src/Storage.js
@@ -41,4 +41,4 @@ class Storage<T> {
   constructor(name: string, version?: number) {
     if (version) {
       const parsedVersion = parseInt(version, 10);
-      if (!parsedVersion || par
+      if (!parsedVersion || parsedVersion <= 0
