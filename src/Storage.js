6d8ba5d8716130a6c167bd88bea8515a34ee950f
ithkuilg <99258753+ithkuilg@users.noreply.github.com>
2022-05-03 16:48:45 +0000

    Added a comment.

--- src/Storage.js
+++ src/Storage.js
@@ -45,4 +45,6 @@ class Storage<T> {
         throw new Error('version has to be a positive integer');
       }
       this.name = name;
-      this.version = parsed
+      this.version = parsedVersion;
+
+   
