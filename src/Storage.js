e8bf5d8666a106e2b69a8b283809515ed38917dd
ithkuilg <99258753+ithkuilg@users.noreply.github.com>
2022-05-05 21:58:40 +0000

    Rearrange.

--- src/Storage.js
+++ src/Storage.js
@@ -50,4 +50,4 @@ class Storage<T> {
       const previousVersion = parseInt(get(name), 10) || 0;
       if (parsedVersion > previousVersion) {
         remove(`${name}:${previousVersion}`);
-        set
+        set(name, version.to
