56180671048316c265c4e4e4484d09b4b8e709ad
ithkuilg <99258753+ithkuilg@users.noreply.github.com>
2022-05-10 19:48:47 +0000

    version 0.1.10

--- src/Storage.js
+++ src/Storage.js
@@ -55,4 +55,6 @@ class Storage<T> {
     } else {
       const existingVersion = parseInt(get(name), 10) || 0;
       if (!existingVersion) {
-        throw new Error(`There is no existing storage named ${name}
+        throw new Error(`There is no existing storage named ${name}`);
+      }
+ 
