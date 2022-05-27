eff59eddc27f7c539842a69edebba80a236e198e
ithkuilg <99258753+ithkuilg@users.noreply.github.com>
2022-05-27 12:21:49 +0000

    NR-408 : Instagram support in LD sources

--- src/__tests__/Storage-test.js
+++ src/__tests__/Storage-test.js
@@ -5,3 +5,20 @@ const STORAGE_NAME = 'test_storage';
 const STORAGE_VERSION = 42;
 const TEST_OBJECT = {
   x: 1,
+  y: ['hello', 'world'],
+  z: false,
+};
+
+let localStorageBackup = null;
+let getItem = jest.fn();
+let setItem = jest.fn();
+let removeItem = jest.fn();
+let clear = jest.fn();
+
+beforeEach(() => {
+  getItem = jest.fn();
+  setItem = jest.fn();
+  removeItem = jest.fn();
+  clear = jest.fn();
+  const localStorageMock = {
+    getItem,
