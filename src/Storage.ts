f1849f74c22e89a50c18e460d94d2bbdb9d3b2ea
ithkuilg <99258753+ithkuilg@users.noreply.github.com>
2022-05-27 07:45:07 +0000

    Fix blocking allocations called very early during bootup

---
+++ src/Storage.ts
@@ -0,0 +1,9 @@
+
+declare class Storage<T> {
+  constructor(name: string, version?: number);
+  read(): T | null;
+  write(value: T): void;
+  static reset(): void;
+}
+
+export default Storage;
---
+++ src/__tests__/Storage-test.js
@@ -0,0 +1,7 @@
+
+const Storage = require('../Storage');
+
+const STORAGE_NAME = 'test_storage';
+const STORAGE_VERSION = 42;
+const TEST_OBJECT = {
+  x: 1,
